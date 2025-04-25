216463209e05bc699982088b6fec04cf19210ab7
OddoMaxi <73769710+OddoMaxi@users.noreply.github.com>
2025-04-25 03:16:59 +0000

    ticket design 4:error fixed 4

--- functions/formatReservationId.js
+++ functions/formatReservationId.js
@@ -3,3 +3,5 @@
 function formatReservationId(eventId, catIdx, ticketNum) {
   return `EVT${eventId}-CAT${catIdx}-T${String(ticketNum).padStart(4, '0')}`;
 }
+
+module.exports = { formatReservationId };
